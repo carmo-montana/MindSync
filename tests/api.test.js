bab59fd904a30746fcab2a54186ca338cd2fef07
fantasma <carmodepp@gmail.com>
2025-02-10 16:12:05 -0300

    m

--- tests/api.test.js
+++
@@ -1,9 +0,0 @@
-import request from "supertest";
-import app from "../server.js"; // Caminho correto
-
-describe("API MindSync - Testes de Endpoints", () => {
-  test("🟢 Deve retornar status 200 na rota /", async () => {
-    const res = await request(app).get("/");
-    expect(res.statusCode).toBe(200);
-  });
-});
