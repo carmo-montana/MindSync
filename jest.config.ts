6dda07bc0d5f6d2703ba95bb688882ccc007abe2
fantasma <carmodepp@gmail.com>
2025-02-10 22:35:43 -0300

    Configuração

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,11 @@
-module.exports = {
-    testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"],
-    testPathIgnorePatterns: ["/node_modules/"],
-    transform: {
-      "^.+\\.ts$": "ts-jest",
-    },
-  };
+import type { Config } from "jest";
+
+const config: Config = {
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"], // Padrão de arquivos de teste
+  testPathIgnorePatterns: ["/node_modules/"], // Ignorar node_modules
+  transform: {
+    "^.+\\.ts$": "ts-jest", // Transformar arquivos TypeScript
+  },
+};
+
+export default config;
